da6e383703564f3bdc009a3c8c2a66add6a46b35
fe0b6 <spirit-dnk@yandex.ru>
2019-03-14 23:11:29 +0300

    add # to end link

--- tools.go
+++ tools.go
@@ -26,7 +26,7 @@ var (
 func init() {
 	GroupAccessTokenReg = regexp.MustCompile("^access_token_([0-9]+)$")
 	linkScreenNameReg = regexp.MustCompile("vk.com/(.+)")
-	LinkDomainReg = regexp.MustCompile("vk.com/([^? \n]+)\\S*")
+	LinkDomainReg = regexp.MustCompile("vk.com/([^? \n#]+)\\S*")
 	ObjLinkReg = regexp.MustCompile("(?:wall|page|topic|photo|album|video|product|market)(-)?([0-9]+)(?:_([0-9]+))?")
 	InternalLinkReg = regexp.MustCompile("\\[(id|club|public|group)([0-9]+)\\|[^\\]]+\\]")
 }
